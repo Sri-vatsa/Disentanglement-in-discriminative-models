3ca29f9d9f4d1ced08087096ca717af0d29f24b8
Srivatsa <p.psrivatsa@gmail.com>
2021-11-26 00:28:02 -0500

    Clean up directory

--- download_simclr.sh
+++
@@ -1,4 +0,0 @@
-# Needed to download pretrained simclr models
-pip3 install wldhx.yadisk-direct
-curl -L $(yadisk-direct https://yadi.sk/d/Sg9uSLfLBMCt5g?w=1) -o pretrained_models.zip
-unzip pretrained_models.zip 
