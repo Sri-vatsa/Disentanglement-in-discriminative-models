a2f5c7ca7df6f756beab883cabf110863fca53ba
Yash <mailsforyashj@gmail.com>
2021-11-26 23:47:23 -0500

    Make regularizer GPU friendly

--- disentanglement.py
+++ disentanglement.py
@@ -23,25 +23,28 @@ def revised_ksg_estimator(variables, k=5, q=float('inf')):
 		
     Output: a scalar representing I(variables[0];variables[1];...variables[N-1])
 	'''
+	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 	if len(variables.size()) == 2:
 		variables = variables[:, :, None]
 
 	assert k <= variables.size()[1] - 1, "Set k smaller than num. samples - 1"
 	hidden_dim, N, d = variables.size()
-	N, d, k = torch.tensor([N, d, k])
+	N, d, k = torch.tensor([N, d, k]).to(device)
 	data = torch.cat([variables[i] for i in range(hidden_dim)], dim=1)
  
 	knn_dis = [torch.norm(data - point, dim=1, p=q).topk(k + 1, largest=False)[0][k] for point in data]
 	ans_all_data = -torch.special.digamma(k) + torch.log(N) + torch_vd(d*hidden_dim, q)
-	ans_individual = torch.tensor([torch.log(N) + torch_vd(d, q) for i in range(hidden_dim)])
+	ans_individual = torch.tensor([torch.log(N) + torch_vd(d, q) for i in range(hidden_dim)]).to(device)
 	
 	for i in range(N):
 		ans_all_data += (d * hidden_dim) * (torch.log(knn_dis[i]) / N)
 		for j in range(hidden_dim):
 			dist = torch.norm(variables[j] - variables[j][i], dim=1, p=q)
-			num_values = torch.tensor(dist[dist < (knn_dis[i] + 1e-15)].size(dim=0) - 1)
+			num_values = torch.tensor(dist[dist < (knn_dis[i] + 1e-15)].size(dim=0) - 1).to(device)
 			ans_individual[j] += -torch.log(num_values) / N \
 			+ d*torch.log(knn_dis[i]) / N
 
 	sum = torch.sum(ans_individual)
+	
 	return torch.abs(sum - ans_all_data)
